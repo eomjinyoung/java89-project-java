efa4ccb05d651c7336db1b4e0541bc054b3076ae
eomjinyoung <jinyoung.eom@gmail.com>
2016-11-29 11:39:45 +0900

    .

--- src/bitcamp/java89/ems/server/context/ApplicationContext.java
+++ src/bitcamp/java89/ems/server/context/ApplicationContext.java
@@ -8,32 +8,109 @@ package bitcamp.java89.ems.server.context;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
-
-import bitcamp.java89.ems.server.AbstractCommand;
 
 public class ApplicationContext {
   HashMap<String,Object> objPool = new HashMap<>();
   
   public ApplicationContext(String[] packages) {
+    ArrayList<Class<?>> classList = getClassList(packages);
+    prepareObjects(classList);
+    injectDependencies();
+  }
+
+  private void injectDependencies() {
+    // HashMap에 저장된 객체 목록을 뽑아 온다.
+    Collection<Object> objects = objPool.values();
+    
+    for (Object obj : objects) {
+      System.out.println(obj.getClass().getName());
+      
+      // 각 객체의 public 메서드 목록을 뽑는다.
+      Method[] methods = obj.getClass().getMethods();
+      
+      for (Method m : methods) {
+        if (!m.getName().startsWith("set")) { // 셋터가 아니면 제외시킨다.
+          continue;
+        }
+        
+        if (m.getParameterCount() != 1) { // 파라미터의 개수가 1개가 아니면 제외시킨다.
+          continue;
+        }
+        
+        // 셋터의 0 번째 파라미터 타입을 알아낸다.
+        Class<?> paramType = m.getParameterTypes()[0];
+        
+        // 그 타입에 해당하는 객체를 objPool에서 찾는다.
+        Object dependency = findDependency(paramType);
+        
+        if (dependency != null) { // 찾았다면, 
+          try {
+            System.out.println("==>" + m.getName());
+            m.invoke(obj, dependency); // 의존 객체를 주입하기 위해 셋터를 호출한다.
+          } catch (Exception e) {}
+        }
+      }
+    }
+    
+  }
+
+  private Object findDependency(Class<?> paramType) {
+    Collection<Object> objects = objPool.values();
+    for (Object obj : objects) {
+      if (paramType.isInstance(obj)){
+        return obj;
+      }
+    }
+    return null;
+  }
+
+  private ArrayList<Class<?>> getClassList(String[] packages) {
+    ArrayList<Class<?>> classList = new ArrayList<>();
+    
     for (String packageName : packages) {
-      // 클래스 파일을 찾을 디렉토리 경로를 정의한다.
-      // 그런데 파라미터로 넘어오는 값은 순수한 패키지 이름
-      // (예: bitcamp.java89.ems.server.controller)으로 되어 있다.
-      // 그래서 다음과 같이 파일 경로로 만들려면 "."을 "/"로 변경해야 한다.
-      // 예) ./bin/bitcamp/java89/ems/server/controller
       try {
-        File dir = new File("./bin/" + packageName.replaceAll("\\.", "/"));
-        System.out.println(dir.getCanonicalPath());
+        findClasses(packageNameToFile(packageName), classList);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+    return classList;
+  }
+
+  private void prepareObjects(ArrayList<Class<?>> classList) {
+    for (Class<?> clazz : classList) {
+      try {
+        Object obj = clazz.newInstance();
+        String key = null;
+        try {
+          Method m = clazz.getMethod("getCommandString");
+          key = (String)m.invoke(obj);
+        } catch (Exception e) {
+          key = clazz.getName();
+        }
+        objPool.put(key, obj);
+        
       } catch (Exception e) {
         e.printStackTrace();
       }
     }
   }
   
-  public void getClasses(File dir, ArrayList<Class<?>> classList) {
+  private File packageNameToFile(String packageName) {
+    // 클래스 파일을 찾을 디렉토리 경로를 정의한다.
+    // 그런데 파라미터로 넘어오는 값은 순수한 패키지 이름
+    // (예: bitcamp.java89.ems.server.controller)으로 되어 있다.
+    // 그래서 다음과 같이 파일 경로로 만들려면 "."을 "/"로 변경해야 한다.
+    // 예) ./bin/bitcamp/java89/ems/server/controller
+    return new File("./bin/" + packageName.replaceAll("\\.", "/"));
+  }
+  
+  private void findClasses(File dir, ArrayList<Class<?>> classList) {
     if (!dir.isDirectory()) {
       return;
     }
@@ -51,11 +128,11 @@ public class ApplicationContext {
     
     for (File file : files) {
       if (file.isDirectory()) {
-        getClasses(file, classList);
+        findClasses(file, classList);
         
       } else {
         try {
-          Class<?> c = getClass(file);
+          Class<?> c = loadClass(file);
           if (!isAbstract(c)) {
             classList.add(c); 
           }
@@ -66,7 +143,7 @@ public class ApplicationContext {
     }
   }
 
-  private Class<?> getClass(File file) throws IOException, ClassNotFoundException {
+  private Class<?> loadClass(File file) throws IOException, ClassNotFoundException {
     String path = file.getCanonicalPath().replaceAll("\\\\", "/")
         .replaceAll(".class", ""); 
     int pos = path.indexOf("/bin/");  
